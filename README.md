7cc678d257c4982fd76ac46efd84cae2d4543722
jitu-malviya <122000576+jitu-malviya@users.noreply.github.com>
2023-03-29 11:43:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mat-select-virtual-scroll-1q9bh6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mat-select-virtual-scroll-9u4rjx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,93 @@
+import {
+  Component,
+  ViewChild,
+  ViewChildren,
+  QueryList,
+  ChangeDetectorRef
+} from "@angular/core";
+import { FormControl } from "@angular/forms";
+import {
+  CdkVirtualScrollViewport,
+  ScrollDispatcher
+} from "@angular/cdk/scrolling";
+import { MatOption } from "@angular/material/core";
+import { filter } from "rxjs/operators";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "test-proj";
+
+  toppings = new FormControl();
+  toppingList: any[] = [];
+  selected: any = [];
+
+  @ViewChild(CdkVirtualScrollViewport, { static: true })
+  cdkVirtualScrollViewPort: CdkVirtualScrollViewport;
+
+  multiSelectControl = new FormControl();
+
+  @ViewChildren(MatOption)
+  options: QueryList<MatOption>;
+
+  constructor(private cd: ChangeDetectorRef, readonly sd: ScrollDispatcher) {
+    for (let i = 0; i < 100000; i++) {
+      this.toppingList.push({ id: i, viewValue: "option-" + i });
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.sd
+      .scrolled()
+      .pipe(filter(scrollable => this.cdkVirtualScrollViewPort === scrollable))
+      .subscribe(() => {
+        let needUpdate = false;
+
+        this.options.forEach(option => {
+          const selected = this.selected.includes(option.value);
+
+          if (selected && !option.selected) {
+            option.select();
+            needUpdate = true;
+          } else if (!selected && option.selected) {
+            option.deselect();
+            needUpdate = true;
+          }
+        });
+
+        if (needUpdate) {
+          this.cd.detectChanges();
+        }
+      });
+  }
+
+  foropen() {
+    this.cdkVirtualScrollViewPort.scrollToIndex(5);
+  }
+
+  openChange($event: boolean) {
+    if ($event) {
+      this.foropen();
+      this.cdkVirtualScrollViewPort.scrollToIndex(0);
+      this.cdkVirtualScrollViewPort.checkViewportSize();
+    }
+  }
+
+  onSelectionChange(change): void {
+    if (!change.isUserInput) {
+      return;
+    }
+    console.log(change.source.value);
+    const value = change.source.value;
+    const idx = this.selected.indexOf(change.source.value);
+
+    if (idx > -1) {
+      this.selected.splice(idx, 1);
+    } else {
+      this.selected.push(value);
+    }
+  }
+}
